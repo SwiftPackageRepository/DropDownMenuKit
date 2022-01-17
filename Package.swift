ef3759a97bd86a17456d952b2966875d402e90d2
Sascha Müllner <sascha.muellner@gmail.com>
2022-01-17 14:32:51 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "DropDownMenuKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "DropDownMenuKit",
+            targets: ["DropDownMenuKit"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "DropDownMenuKit",
+            dependencies: [],
+        	path: ".",
+        	exclude: ["Example"],
+   			resources: [
+   				.copy("DropDownMenuKit.xcassets")
+   			],
+        	cSettings: [
+      			.headerSearchPath("./")
+   			]
+        )
+    ]
+)
